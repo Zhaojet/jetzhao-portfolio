4e5ce3cbd40a7b7ec68990c1da39192f05b38739
Zhaojet <145517895+Zhaojet@users.noreply.github.com>
2025-08-01 15:31:20 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,38 +5,38 @@ module.exports = defineConfig({
   
   // 生产环境配置
   configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // 代码分割优化
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          cacheGroups: {
-            // 第三方库单独打包
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all',
-              priority: 10
-            },
-            // Element Plus 单独打包
-            elementPlus: {
-              test: /[\\/]node_modules[\\/]element-plus[\\/]/,
-              name: 'element-plus',
-              chunks: 'all',
-              priority: 20
-            },
-            // Vue 相关库单独打包
-            vue: {
-              test: /[\\/]node_modules[\\/](vue|vue-router)[\\/]/,
-              name: 'vue-libs',
-              chunks: 'all',
-              priority: 15
-            }
-          }
-        }
-      }
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   // 代码分割优化
+    //   config.optimization = {
+    //     ...config.optimization,
+    //     splitChunks: {
+    //       chunks: 'all',
+    //       cacheGroups: {
+    //         // 第三方库单独打包
+    //         vendor: {
+    //           test: /[\\/]node_modules[\\/]/,
+    //           name: 'vendors',
+    //           chunks: 'all',
+    //           priority: 10
+    //         },
+    //         // Element Plus 单独打包
+    //         elementPlus: {
+    //           test: /[\\/]node_modules[\\/]element-plus[\\/]/,
+    //           name: 'element-plus',
+    //           chunks: 'all',
+    //           priority: 20
+    //         },
+    //         // Vue 相关库单独打包
+    //         vue: {
+    //           test: /[\\/]node_modules[\\/](vue|vue-router)[\\/]/,
+    //           name: 'vue-libs',
+    //           chunks: 'all',
+    //           priority: 15
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   },
 
   // CSS 优化
